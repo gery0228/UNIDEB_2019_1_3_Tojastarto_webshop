b329c2f2e34fbd5875ee06b02a01b47da04a5d3a
gery0228 <gery980228@gmail.com>
2019-11-30 22:45:18 +0100

    Előző és Következő gombok hozzáadva.

--- Webshop/body.php
+++ Webshop/body.php
@@ -18,3 +18,15 @@
         <img src="img/tel.jpg" alt="Los Angeles" style="width:100%;">
         
       </div>
+	  
+	  <!-- Left and right controls -->
+    <a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only" >Előző</span>
+    </a>
+    <a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Következő</span>
+    </a>
+  </div>
+</div>
