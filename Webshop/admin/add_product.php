101de1ca9ee12db86d954b109eed39ce85fd661a
ktomi26 <ktomi200@gmail.com>
2019-11-30 21:14:34 +0100

    Képek megjelenítéséért felelős függvény hozzáadása.

--- Webshop/admin/add_product.php
+++ Webshop/admin/add_product.php
@@ -12,3 +12,8 @@ $c_price=$_POST['c_price'];
 $product_type=$_POST['product_type'];
 $brand=$_POST['brand'];
 $tags=$_POST['tags'];
+
+$picture_name=$_FILES['picture']['name'];
+$picture_type=$_FILES['picture']['type'];
+$picture_tmp_name=$_FILES['picture']['tmp_name'];
+$picture_size=$_FILES['picture']['size'];
