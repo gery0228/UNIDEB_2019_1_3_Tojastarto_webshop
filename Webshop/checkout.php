c0088b12261a52c5069da07e2824ee6015c5e91c
gery0228 <gery980228@gmail.com>
2019-11-30 23:03:07 +0100

    Adatbázis kezelő rész hozzáadva a checkout.php-ban.

--- Webshop/checkout.php
+++ Webshop/checkout.php
@@ -101,3 +101,179 @@ span.price {
   }
 }
 </style>
+
+
+<section class="section">       
+	<div class="container-fluid">
+		<div class="row-checkout">
+		<?php
+		if(isset($_SESSION["uid"])){
+			$sql = "SELECT * FROM user_info WHERE user_id='$_SESSION[uid]'";
+			$query = mysqli_query($con,$sql);
+			$row=mysqli_fetch_array($query);
+		
+		echo'
+			<div class="col-75">
+				<div class="container-checkout">
+				<form id="checkout_form" action="checkout_process.php" method="POST" class="was-validated">
+
+					<div class="row-checkout">
+					
+					<div class="col-50">
+						<h3>Szállítási cím</h3>
+						<label for="fname"><i class="fa fa-user" ></i> Név</label>
+						<input type="text" id="fname" class="form-control" name="firstname" pattern="^[a-zA-Z ]+$"  value="'.$row["first_name"].' '.$row["last_name"].'">
+						<label for="email"><i class="fa fa-envelope"></i> Email</label>
+						<input type="text" id="email" name="email" class="form-control" pattern="^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9]+(\.[a-z]{2,4})$" value="'.$row["email"].'" required>
+						<label for="adr"><i class="fa fa-address-card-o"></i> Cím</label>
+						<input type="text" id="adr" name="address" class="form-control" value="'.$row["address1"].'" required>
+						<label for="city"><i class="fa fa-institution"></i> Város</label>
+						<input type="text" id="city" name="city" class="form-control" value="'.$row["address2"].'" pattern="^[a-zA-Z ]+$" required>
+
+						<div class="row">
+						<div class="col-50">
+							<label for="state">Ország</label>
+							<input type="text" id="state" name="state" class="form-control" pattern="^[a-zA-Z ]+$" required>
+						</div>
+						<div class="col-50">
+							<label for="zip">Irányítószám</label>
+							<input type="text" id="zip" name="zip" class="form-control" pattern="^[0-9]{6}(?:-[0-9]{4})?$" required>
+						</div>
+						</div>
+					</div>
+					
+					
+					<div class="col-50">
+						<h3>Fizetés</h3>
+						<label for="fname">Elfogadott kártyák</label>
+						<div class="icon-container">
+						<i class="fa fa-cc-visa" style="color:navy;"></i>
+						<i class="fa fa-cc-amex" style="color:blue;"></i>
+						<i class="fa fa-cc-mastercard" style="color:red;"></i>
+						<i class="fa fa-cc-discover" style="color:orange;"></i>
+						</div>
+						
+						
+						<label for="cname">Kártyatulajdonos neve</label>
+						<input type="text" id="cname" name="cardname" class="form-control" pattern="^[a-zA-Z ]+$" required>
+						
+						<div class="form-group" id="card-number-field">
+                        <label for="cardNumber">Kártyaszám</label>
+                        <input type="text" class="form-control" id="cardNumber" name="cardNumber" required>
+                    </div>
+						<label for="expdate">Lejárati dátum</label>
+						<input type="text" id="expdate" name="expdate" class="form-control" pattern="^((0[1-9])|(1[0-2]))\/(\d{2})$" placeholder="12/22"szükséges>
+						
+
+						<div class="row">
+						
+						<div class="col-50">
+							<div class="form-group CVV">
+								<label for="cvv">CVV</label>
+								<input type="text" class="form-control" name="cvv" id="cvv" required>
+						</div>
+						</div>
+					</div>
+					</div>
+					</div>
+					<label><input type="CHECKBOX" name="q" class="roomselect" value="conform" required> Szállítási cím ugyan az mint a számlázási cím
+					</label>';
+					$i=1;
+					$total=0;
+					$total_count=$_POST['total_count'];
+					while($i<=$total_count){
+						$item_name_ = $_POST['item_name_'.$i];
+						$amount_ = $_POST['amount_'.$i];
+						$quantity_ = $_POST['quantity_'.$i];
+						$total=$total+$amount_ ;
+						$sql = "SELECT product_id FROM products WHERE product_title='$item_name_'";
+						$query = mysqli_query($con,$sql);
+						$row=mysqli_fetch_array($query);
+						$product_id=$row["product_id"];
+						echo "	
+						<input type='hidden' name='prod_id_$i' value='$product_id'>
+						<input type='hidden' name='prod_price_$i' value='$amount_'>
+						<input type='hidden' name='prod_qty_$i' value='$quantity_'>
+						";
+						$i++;
+					}
+					
+				echo'	
+				<input type="hidden" name="total_count" value="'.$total_count.'">
+					<input type="hidden" name="total_price" value="'.$total.'">
+					
+					<input type="submit" id="submit" value="Folytatás a kifizetéshez" class="checkout-btn">
+				</form>
+				</div>
+			</div>
+			';
+		}else{
+			echo"<script>window.location.href = 'cart.php'</script>";
+		}
+		?>
+
+			<div class="col-25">
+				<div class="container-checkout">
+				
+				<?php
+				if (isset($_POST["cmd"])) {
+				
+					$user_id = $_POST['custom'];
+					
+					
+					$i=1;
+					echo
+					"
+					<h4>Kosár
+					<span class='price' style='color:black'>
+					<i class='fa fa-shopping-cart'></i> 
+					<b>$total_count</b>
+					</span>
+				</h4>
+
+					<table class='table table-condensed'>
+					<thead><tr>
+					<th >Sorszám</th>
+					<th >Termék neve</th>
+					<th >	Mennyiség	</th>
+					<th >	Ár</th></tr>
+					</thead>
+					<tbody>
+					";
+					$total=0;
+					while($i<=$total_count){
+						$item_name_ = $_POST['item_name_'.$i];
+						
+						$item_number_ = $_POST['item_number_'.$i];
+						
+						$amount_ = $_POST['amount_'.$i];
+						
+						$quantity_ = $_POST['quantity_'.$i];
+						$total=$total+$amount_ ;
+						$sql = "SELECT product_id FROM products WHERE product_title='$item_name_'";
+						$query = mysqli_query($con,$sql);
+						$row=mysqli_fetch_array($query);
+						$product_id=$row["product_id"];
+					
+						echo "	
+
+						<tr><td><p>$item_number_</p></td><td><p>$item_name_</p></td><td ><p>$quantity_</p></td><td ><p>$amount_</p></td></tr>";
+						
+						$i++;
+					}
+
+				echo"
+
+				</tbody>
+				</table>
+				<hr>
+				
+				<h3>total<span class='price' style='color:black'><b>$$total</b></span></h3>";
+					
+				}
+				?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
