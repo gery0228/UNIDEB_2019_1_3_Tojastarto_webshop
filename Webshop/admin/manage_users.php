ccff06ac25fea5affc6921a2ce2b93e7896dbe05
ktomi26 <ktomi200@gmail.com>
2019-11-30 21:35:47 +0100

    HTML script hozzáadása.

---
+++ Webshop/admin/manage_users.php
@@ -0,0 +1,25 @@
+<?php
+
+include("../db.php");
+if(isset($_GET['action']) && $_GET['action']!="" && $_GET['action']=='delete')
+{
+$user_id=$_GET['user_id'];
+
+/*this is delet quer*/
+mysqli_query($con,"delete from user_info where user_id='$user_id'")or die("query is incorrect...");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Adminisztrációs felület</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="style/css/bootstrap.min.css" rel="stylesheet">
+<link href="style/css/k.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+</head>
+<body>
+<?php include("include/header.php"); ?>
+
+<div class="container-fluid">
