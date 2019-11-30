7a1dc821d76996058efaa4fe35b13b41cf20a19b
gery0228 <gery980228@gmail.com>
2019-11-30 22:44:06 +0100

    body.php-ban képek hozzáadása a slidokhoz.

---
+++ Webshop/body.php
@@ -0,0 +1,20 @@
+<div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="img/lg.jpg" alt="Los Angeles" style="width:100%;">
+
+        
+      </div>
+
+      <div class="item">
+        <img src="img/tel.jpg" alt="Los Angeles" style="width:100%;">
+        
+      </div>
