03317ec6b3eee0167477137d8ad2ee16bf0f3f60
ktomi26 <ktomi200@gmail.com>
2019-11-30 21:42:12 +0100

    Adatbázis hozzáadása.

---
+++ Webshop/admin/orders.php
@@ -0,0 +1,24 @@
+<?php
+
+include("../db.php");
+error_reporting(0);
+if(isset($_GET['action']) && $_GET['action']!="" && $_GET['action']=='delete')
+{
+$order_id=$_GET['order_id'];
+
+/*this is delet query*/
+mysqli_query($con,"delete from orders where order_id='$order_id'")or die("delete query is incorrect...");
+} 
+
+///pagination
+$page=$_GET['page'];
+
+if($page=="" || $page=="1")
+{
+$page1=0;	
+}
+else
+{
+$page1=($page*10)-10;	
+}
+?>
