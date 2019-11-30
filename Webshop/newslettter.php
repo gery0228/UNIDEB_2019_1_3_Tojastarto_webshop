a390de78f59fc225e9edd374f4d26e5aee7709cb
gery0228 <gery980228@gmail.com>
2019-11-30 22:39:20 +0100

    newslettter.php hozzáadva a webáruházhoz.

---
+++ Webshop/newslettter.php
@@ -0,0 +1,41 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Iratkozz fel az <strong>Újdonságokért</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Add meg az emailed">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Feliratkozás</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="hhttps://github.com/gery0228/UNIDEB_2019_1_3_Tojastarto_webshop"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/gery0228/UNIDEB_2019_1_3_Tojastarto_webshop"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/gery0228/UNIDEB_2019_1_3_Tojastarto_webshop"><i class="fa fa-instagram"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/gery0228/UNIDEB_2019_1_3_Tojastarto_webshop"><i class="fa fa-github"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
