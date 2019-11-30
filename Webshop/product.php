170f6d5196d40be2e86068cd8951395f85185920
gery0228 <gery980228@gmail.com>
2019-11-30 23:20:50 +0100

    product.php árak lekérdezése az adatbázisból rész bővítése.

--- Webshop/product.php
+++ Webshop/product.php
@@ -405,3 +405,85 @@ include "header.php";
 									}
 								} 
 								?>
+								<?php
+                    include 'db.php';
+								$product_id = $_GET['p'];
+                    
+					$product_query = "SELECT * FROM products,categories WHERE product_cat=cat_id AND product_id BETWEEN $product_id AND $product_id+3";
+                $run_query = mysqli_query($con,$product_query);
+                if(mysqli_num_rows($run_query) > 0){
+
+                    while($row = mysqli_fetch_array($run_query)){
+                        $pro_id    = $row['product_id'];
+                        $pro_cat   = $row['product_cat'];
+                        $pro_brand = $row['product_brand'];
+                        $pro_title = $row['product_title'];
+                        $pro_price = $row['product_price'];
+                        $pro_image = $row['product_image'];
+
+                        $cat_name = $row["cat_title"];
+
+                        echo "
+				
+                        
+                                <div class='col-md-3 col-xs-6'>
+								<a href='product.php?p=$pro_id'><div class='product'>
+									<div class='product-img'>
+										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+										<div class='product-label'>
+											<span class='sale'>-30%</span>
+											<span class='new'>Újdonság</span>
+										</div>
+									</div></a>
+									<div class='product-body'>
+										<p class='product-category'>$cat_name</p>
+										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
+										<h4 class='product-price header-cart-item-info'>$pro_price<del class='product-old-price'>$990.00</del></h4>
+										<div class='product-rating'>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+										</div>
+										<div class='product-btns'>
+											<button class='add-to-wishlist'><i class='fa fa-heart-o'></i><span class='tooltipp'>Kívánságlistához adás</span></button>
+											<button class='add-to-compare'><i class='fa fa-exchange'></i><span class='tooltipp'>Összehasonlítás</span></button>
+											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>Gyorsnézet</span></button>
+										</div>
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> Kosárba rakás</button>
+									</div>
+								</div>
+                                </div>
+							
+                        
+			";
+		}
+        ;
+      
+}
+?>
+					<!-- product -->
+					
+					<!-- /product -->
+
+				</div>
+				<!-- /row -->
+                
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /Section -->
+
+		<!-- NEWSLETTER -->
+		
+		<!-- /NEWSLETTER -->
+
+		<!-- FOOTER -->
+<?php
+include "newslettter.php";
+include "footer.php";
+
+?>
