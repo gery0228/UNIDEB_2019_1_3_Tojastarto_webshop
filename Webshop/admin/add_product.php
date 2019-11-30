700064c6f6ae8da17506404f27be6b5dd330e1b9
ktomi26 <ktomi200@gmail.com>
2019-11-30 21:13:24 +0100

    Termék TAG-ek hozzáadása.

--- Webshop/admin/add_product.php
+++ Webshop/admin/add_product.php
@@ -2,3 +2,13 @@
 
 include("../db.php");
 session_start();
+
+if(isset($_POST['submit']))
+{
+$product_name=$_POST['product_name'];
+$details=$_POST['details'];
+$price=$_POST['price'];
+$c_price=$_POST['c_price'];
+$product_type=$_POST['product_type'];
+$brand=$_POST['brand'];
+$tags=$_POST['tags'];
