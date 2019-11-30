c27c42ec6994ef0dc19a2bbdefcbc2c2fe27ddc3
gery0228 <gery980228@gmail.com>
2019-11-30 23:11:43 +0100

    Scrolling script hozzáadva a product.php-ban.

---
+++ Webshop/product.php
@@ -0,0 +1,12 @@
+<?php
+include "header.php";
+?>
+		<!-- /BREADCRUMB -->
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
+				});
+			});
+</script>
