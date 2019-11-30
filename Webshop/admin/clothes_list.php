bbd13e16deffe075abeb43fbd95e3417edecb276
ktomi26 <ktomi200@gmail.com>
2019-11-30 21:48:41 +0100

    HTML script hozzáadása.

--- Webshop/admin/clothes_list.php
+++ Webshop/admin/clothes_list.php
@@ -35,3 +35,69 @@ else
 $page1=($page*10)-10;	
 } 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Admin Panel</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="style/css/bootstrap.min.css" rel="stylesheet">
+<link href="style/css/k.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+</head>
+<body>
+  <?php include("include/header.php");?>
+   	<div class="container-fluid main-container">
+	<?php include("include/side_bar.php");?>
+    <div class="col-md-9 content" style="margin-left:10px">
+    <div class="panel-heading" style="background-color:#c4e17f">
+	<h1>Termék lista  <?php echo $page;?> </h1></div><br>
+<div class='table-responsive'>  
+<div style="overflow-x:scroll;">
+<table class="table  table-hover table-striped" style="font-size:18px">
+<tr><th>Kép</th><th>Név</th><th>Ár</th><th>
+	<a class=" btn btn-primary" href="add_product.php">Új hozzáadás</a></th></tr>
+<?php 
+
+$result=mysqli_query($con,"select product_id,product_image, product_title,product_price from products  where  product_cat=2 or product_cat=3 or product_cat=4 Limit $page1,10")or die ("query 1 incorrect.....");
+
+while(list($product_id,$image,$product_name,$price)=mysqli_fetch_array($result))
+{
+echo "<tr><td><img src='../product_images/$image' style='width:50px; height:50px; border:groove #000'></td><td>$product_name</td>
+<td>$price</td>
+<td>
+
+<a class=' btn btn-success' href='clothes_list.php?product_id=$product_id&action=delete'>Delete</a>
+</td></tr>";
+}
+
+?>
+</table>
+</div></div>
+
+<nav align="center">
+  
+
+<?php 
+
+
+$paging=mysqli_query($con,"select product_id,product_image, product_title,product_price from products");
+$count=mysqli_num_rows($paging);
+
+$a=$count/10;
+$a=ceil($a);
+echo "<bt>";echo "<bt>";
+for($b=1; $b<=$a;$b++)
+{
+?> 
+<ul class="pagination" style="border:groove #666">
+<li><a class="label-info" href="clothes_list.php?page=<?php echo $b;?>"><?php echo $b." ";?></a></li></ul>
+<?php	
+}
+?>
+</nav>
+</div></div>
+<?php include("include/js.php");?>
+</body>
+</html>
