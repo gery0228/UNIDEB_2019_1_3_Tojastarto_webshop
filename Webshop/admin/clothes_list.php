f2f088e9b6ba5cbd11d0c9bedb312528a9447ffb
ktomi26 <ktomi200@gmail.com>
2019-11-30 21:46:27 +0100

    Adatbázisban szereplő képek elérése.

---
+++ Webshop/admin/clothes_list.php
@@ -0,0 +1,37 @@
+<?php
+
+include("../db.php");
+error_reporting(0);
+if(isset($_GET['action']) && $_GET['action']!="" && $_GET['action']=='delete')
+{
+$product_id=$_GET['product_id'];
+
+$result=mysqli_query($con,"select product_image from products where product_id='$product_id'")
+or die("query is incorrect...");
+
+list($picture)=mysqli_fetch_array($result);
+$path="../product_images/$picture";
+
+if(file_exists($path)==true)
+{
+  unlink($path);
+}
+else
+{}
+
+mysqli_query($con,"delete from products where product_id='$product_id'")or die("query is incorrect...");
+}
+
+
+
+$page=$_GET['page'];
+
+if($page=="" || $page=="1")
+{
+$page1=0;	
+}
+else
+{
+$page1=($page*10)-10;	
+} 
+?>
