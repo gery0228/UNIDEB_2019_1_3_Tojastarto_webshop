03eac31db2c34cadf1a37dfe65c371f3ad5f559d
csmate79 <csicsekmate39@gmail.com>
2019-12-01 13:24:17 +0100

    Tárgy törlése a kocsiból

--- Webshop/action.php
+++ Webshop/action.php
@@ -487,5 +487,22 @@ if (isset($_POST["Common"])) {
 	
 	
 }
+
+//Remove Item From cart
+if (isset($_POST["removeItemFromCart"])) {
+	$remove_id = $_POST["rid"];
+	if (isset($_SESSION["uid"])) {
+		$sql = "DELETE FROM cart WHERE p_id = '$remove_id' AND user_id = '$_SESSION[uid]'";
+	}else{
+		$sql = "DELETE FROM cart WHERE p_id = '$remove_id' AND ip_add = '$ip_add'";
+	}
+	if(mysqli_query($con,$sql)){
+		echo "<div class='alert alert-danger'>
+						<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						<b>Product is removed from cart</b>
+				</div>";
+		exit();
+	}
+}
 ?>
 
