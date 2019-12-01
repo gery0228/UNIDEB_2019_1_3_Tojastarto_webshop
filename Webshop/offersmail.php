3ded3d42fed1d568fea921189453e84fee125638
csmate79 <csicsekmate39@gmail.com>
2019-12-01 14:15:14 +0100

    Emaillekérés, ellenzőrzés.

--- Webshop/offersmail.php
+++ Webshop/offersmail.php
@@ -3,5 +3,17 @@
 session_start();
 include "db.php";
 
+if (isset($_POST["email"])) {
+    $email = $_POST['email'];
+    $emailValidation = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9]+(\.[a-z]{2,4})$/";
+    
+    if(empty($email)){
+        echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>Töltsd ki a mezőt!</b>
+			</div>
+		";
+		exit();
+    }else
 
 ?>
