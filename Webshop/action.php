df34614d8be7fe9da45894cb8abc94a464039ef0
csmate79 <csicsekmate39@gmail.com>
2019-12-01 13:12:42 +0100

    mysql bevásárlókocsi.

--- Webshop/action.php
+++ Webshop/action.php
@@ -216,5 +216,39 @@ if(isset($_POST["get_seleted_Category"]) || isset($_POST["selectBrand"]) || isse
 		}
 	}
 
+	if(isset($_POST["addToCart"])){
+		
+
+		$p_id = $_POST["proId"];
+		
+
+		if(isset($_SESSION["uid"])){
+
+		$user_id = $_SESSION["uid"];
+
+		$sql = "SELECT * FROM cart WHERE p_id = '$p_id' AND user_id = '$user_id'";
+		$run_query = mysqli_query($con,$sql);
+		$count = mysqli_num_rows($run_query);
+		if($count > 0){
+			echo "
+				<div class='alert alert-warning'>
+						<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						<b>A termék már a kosárban van, vásárlás folytatása..!</b>
+				</div>
+			";
+		} else {
+			$sql = "INSERT INTO `cart`
+			(`p_id`, `ip_add`, `user_id`, `qty`) 
+			VALUES ('$p_id','$ip_add','$user_id','1')";
+			if(mysqli_query($con,$sql)){
+				echo "
+					<div class='alert alert-success'>
+						<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						<b>Product is Added..!</b>
+					</div>
+				";
+			}
+		}
+		}else{
 ?>
 
