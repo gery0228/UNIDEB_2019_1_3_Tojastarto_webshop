998c21003938468e5ca38439038e7aa43e15552e
csmate79 <csicsekmate39@gmail.com>
2019-12-01 13:34:56 +0100

    Adatbázishoz csatlakozás, felh. regisztráció, ellenőrzés..

--- Webshop/config.php
+++ Webshop/config.php
@@ -1,3 +1,48 @@
 <?php
 
+session_start();
+
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array(); 
+
+
+// connect to the database
+define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'PuneethReddy');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'ecommerece');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+	
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+
+
+
+
+
+
 ?>
