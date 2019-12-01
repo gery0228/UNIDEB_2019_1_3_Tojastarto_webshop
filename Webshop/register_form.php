19d7e5c2358181e6c5cced796315e700d908056e
csmate79 <csicsekmate39@gmail.com>
2019-12-01 13:52:16 +0100

    kiegészítés

--- Webshop/register_form.php
+++ Webshop/register_form.php
@@ -1,3 +1,45 @@
     <div class="wait overlay">
         <div class="loader"></div>
     </div>
+	
+	<!-- row -->
+				
+                <div class="container-fluid">
+					
+						
+						
+						<!-- /Billing Details -->
+						
+								<form id="signup_form" onsubmit="return false" class="login100-form">
+									<div class="billing-details jumbotron">
+									
+                                    <div class="section-title">
+                                        <h2 class="login100-form-title p-b-49" >Regisztrálj itt</h2>
+                                    </div>
+									
+                                    <div class="form-group ">
+                                    
+                                        <input class="input form-control input-borders" type="text" name="f_name" id="f_name" placeholder="Keresztnév">
+                                    </div>
+									
+                                    <div class="form-group">
+                                    
+                                        <input class="input form-control input-borders" type="text" name="l_name" id="l_name" placeholder="Vezetéknév">
+                                    </div>
+									
+                                    <div class="form-group">
+									
+                                        <input class="input form-control input-borders" type="email" name="email"  placeholder="Email">
+                                    </div>
+									
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="password" name="password" id="password" placeholder="Jelszó">
+                                    </div>
+									
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="password" name="repassword" id="repassword" placeholder="Megerősített jelszó">
+                                    </div>
+									
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="text" name="mobile" id="mobile" placeholder="Telefonszám">
+                                    </div>
