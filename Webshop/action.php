01e8965c161f387999e9aa7f37364f2af35a849a
csmate79 <csicsekmate39@gmail.com>
2019-12-01 13:14:52 +0100

    Felhasználó kocsijának a tartalmának a megszámolása

--- Webshop/action.php
+++ Webshop/action.php
@@ -279,5 +279,22 @@ if(isset($_POST["get_seleted_Category"]) || isset($_POST["selectBrand"]) || isse
 		
 		
 	}
+	
+	//Count User cart item
+	if (isset($_POST["count_item"])) {
+	//When user is logged in then we will count number of item in cart by using user session id
+	if (isset($_SESSION["uid"])) {
+		$sql = "SELECT COUNT(*) AS count_item FROM cart WHERE user_id = $_SESSION[uid]";
+	}else{
+		//When user is not logged in then we will count number of item in cart by using users unique ip address
+		$sql = "SELECT COUNT(*) AS count_item FROM cart WHERE ip_add = '$ip_add' AND user_id < 0";
+	}
+	
+	$query = mysqli_query($con,$sql);
+	$row = mysqli_fetch_array($query);
+	echo $row["count_item"];
+	exit();
+	}
+	//Count User cart item
 ?>
 
