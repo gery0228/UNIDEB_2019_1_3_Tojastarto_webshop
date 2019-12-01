7494c81e59ad60662064f1e1fe25a025df2de23d
Barbequeee <kismacsek198@gmail.com>
2019-12-01 21:41:07 +0100

    register.php bővítése

--- Webshop/register.php
+++ Webshop/register.php
@@ -4,3 +4,8 @@ session_start();
 include "db.php";
 if (isset($_POST["f_name"])) 
 {
+	$f_name = $_POST["f_name"];
+	$l_name = $_POST["l_name"];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$repassword = $_POST['repassword'];
