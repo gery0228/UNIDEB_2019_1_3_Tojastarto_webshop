3a237cdf7572ca362ae98985b90ec2d8a907c229
Barbequeee <kismacsek198@gmail.com>
2019-12-01 20:34:11 +0100

    payment_success.php bővítése

--- Webshop/payment_success.php
+++ Webshop/payment_success.php
@@ -6,3 +6,11 @@ if(!isset($_SESSION["uid"])){
 }
 
 if (isset($_GET["st"])) {
+
+	# code...
+	$trx_id = $_GET["tx"];
+		$p_st = $_GET["st"];
+		$amt = $_GET["amt"];
+		$cc = $_GET["cc"];
+		$cm_user_id = $_GET["cm"];
+		$c_amt = $_COOKIE["ta"];
