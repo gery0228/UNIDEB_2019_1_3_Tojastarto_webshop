7e1d50c818d8d2d43d95c6cdde1f39493a4901c6
Barbequeee <kismacsek198@gmail.com>
2019-12-01 21:41:36 +0100

    register.php bővítése

--- Webshop/register.php
+++ Webshop/register.php
@@ -9,3 +9,8 @@ if (isset($_POST["f_name"]))
 	$email = $_POST['email'];
 	$password = $_POST['password'];
 	$repassword = $_POST['repassword'];
+	$mobile = $_POST['mobile'];
+	$address1 = $_POST['address1'];
+	$address2 = $_POST['address2'];
+	$name = "/^[a-zA-Z ]+$/";
+	$emailValidation = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9]+(\.[a-z]{2,4})$/";
